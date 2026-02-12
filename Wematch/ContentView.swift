4b585990155ce5d4c7cac8362991aae9c63f3151
Ramdam17 <remy.ramadour.labs@gmail.com>
2026-02-11 21:28:48 -0500

    Initial Commit

---
+++ Wematch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wematch
+//
+//  Created by Remy Ramadour on 2026-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wematch/WematchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WematchApp.swift
+//  Wematch
+//
+//  Created by Remy Ramadour on 2026-02-11.
+//
+
+import SwiftUI
+
+@main
+struct WematchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
